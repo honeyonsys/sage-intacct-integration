411919d2fc523864443b1644a24ae412320b4dfd
honeyonsys@gmail.com <honeyonsys@gmail.com>
2024-04-08 18:26:21 +0530

    initial code

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Integration of sage intacct</title>
+</head>
+<body>
+Sage intacct 
+</body>
+</html>
